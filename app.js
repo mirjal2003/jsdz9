2bac34d9875ef92f687dca6efefb00ec60898b8f
mirjalol <arasovmisa7@gmail.com>
2023-10-12 14:05:21 +0500

    jsdz9

---
+++ app.js
@@ -0,0 +1,70 @@
+
+let colorRed = document.querySelector('.color__red')
+let colorYellow = document.querySelector ('.color__yellow')
+let colorBlue = document.querySelector('.color__blue')
+let colorBlack = document.querySelector('.color__black')
+let colorNo =document.querySelector('.color__no')
+let colorKvad = document.querySelector('.color__kvad')
+let colorKvad2 = document.querySelector('.color__kvad2')
+let colorKvad3 = document.querySelector('.color__kvad3')
+let colorKvad4 = document.querySelector('.color__kvad4')
+let colorKvad5 = document.querySelector('.color__kvad5')
+let colorKvad6 = document.querySelector('.color__kvad6')
+let colorKvad7 = document.querySelector('.color__kvad7')
+let colorKvad8 = document.querySelector('.color__kvad8')
+
+
+
+
+colorRed.addEventListener('click',() =>{
+    colorKvad.style.backgroundColor = 'red'
+    colorKvad2.style.backgroundColor = 'red'
+    colorKvad3.style.backgroundColor = 'red'
+    colorKvad4.style.backgroundColor = 'red'
+    colorKvad5.style.backgroundColor = 'red'
+    colorKvad6.style.backgroundColor = 'red'
+    colorKvad7.style.backgroundColor = 'red'
+    colorKvad8.style.backgroundColor = 'red'
+})
+colorYellow.addEventListener('click',() =>{
+    colorKvad.style.backgroundColor = 'yellow'
+    colorKvad2.style.backgroundColor = 'yellow'
+    colorKvad3.style.backgroundColor = 'yellow'
+    colorKvad4.style.backgroundColor = 'yellow'
+    colorKvad5.style.backgroundColor = 'yellow'
+    colorKvad6.style.backgroundColor = 'yellow'
+    colorKvad7.style.backgroundColor = 'yellow'
+    colorKvad8.style.backgroundColor = 'yellow'
+})
+colorBlue.addEventListener('click',() =>{
+    colorKvad.style.backgroundColor = 'blue'
+    colorKvad2.style.backgroundColor = 'blue'
+    colorKvad3.style.backgroundColor = 'blue'
+    colorKvad4.style.backgroundColor = 'blue'
+    colorKvad5.style.backgroundColor = 'blue'
+    colorKvad6.style.backgroundColor = 'blue'
+    colorKvad7.style.backgroundColor = 'blue'
+    colorKvad8.style.backgroundColor = 'blue'
+})
+colorBlack.addEventListener('click',() =>{
+    colorKvad.style.backgroundColor = 'black'
+    colorKvad2.style.backgroundColor = 'black'
+    colorKvad3.style.backgroundColor = 'black'
+    colorKvad4.style.backgroundColor = 'black'
+    colorKvad5.style.backgroundColor = 'black'
+    colorKvad6.style.backgroundColor = 'black'
+    colorKvad7.style.backgroundColor = 'black'
+    colorKvad8.style.backgroundColor = 'black'
+})
+    
+
+ colorNo.addEventListener('click',()=>{
+    colorKvad.style.backgroundColor = ''
+    colorKvad2.style.backgroundColor = ''
+    colorKvad3.style.backgroundColor = ''
+    colorKvad4.style.backgroundColor = ''
+    colorKvad5.style.backgroundColor = ''
+    colorKvad6.style.backgroundColor = ''
+    colorKvad7.style.backgroundColor = ''
+    colorKvad8.style.backgroundColor = ''
+ })
